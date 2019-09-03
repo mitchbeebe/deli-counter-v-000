7e4743813eff1c9e2807d385d61adb6433211f20
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-03 02:04:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,12 @@
 # Write your code here.
 def line
-  
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
+    custs = []
+    katz_deli.each_with_index do |cust, idx|
+      custs << "#{idx + 1}. #{cust}"
+    end
     puts "The line is currently: " + custs.join(" ")
   end
 end
