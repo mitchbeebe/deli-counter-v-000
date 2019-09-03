971cf598c79c07e5b9f7f8daee7d9782583ac17e
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-09-03 02:07:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,17 @@
 # Write your code here.
-def line(katz_deli)
-  if katz_deli.size == 0
+def line(deli)
+  if deli.size == 0
     puts "The line is currently empty."
   else
     custs = []
-    katz_deli.each_with_index do |cust, idx|
+    deli.each_with_index do |cust, idx|
       custs << "#{idx + 1}. #{cust}"
     end
     puts "The line is currently: " + custs.join(" ")
   end
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, " + name + ". You are number #{deli.size + 1} in line."
 end
